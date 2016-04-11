c680042d23032e6017c433c8b640d0e716de106b
DeJw92 <dpawlak1992@gmail.com>
2016-04-11 23:56:19 +0200

    mock list with games added

--- app/home-page.ts
+++ app/home-page.ts
@@ -1,8 +1,10 @@
 import {Component} from "angular2/core";
+import {OFFERS} from "./offers-mock-service";
 
 @Component({
     selector: 'home-page',
     templateUrl: 'views/home-page.html'
 })
 export class HomePageComponent {
+    offers = OFFERS;
 }
---
+++ app/offers-mock-service.ts
@@ -0,0 +1,2 @@
+import {Offer} from "./offer";
+export var OFFERS: Offer[] = [new Offer("GTA 4",30), new Offer("Crysis",50), new Offer("Battlefied",40)];
