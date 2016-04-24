03a1970fb94515139c935e75a5395b6a85792f38
DeJw92 <dpawlak1992@gmail.com>
2016-04-24 20:39:43 +0200

    products form added

--- app/products.ts
+++ app/products.ts
@@ -1,9 +1,16 @@
 import {Component} from "angular2/core";
+import {FORM_DIRECTIVES} from "angular2/common";
+import {Offer} from "./offer";
 
 @Component({
-    template: '<h1>Products</h1>',
-    selector: 'products'
+    templateUrl: 'views/products-form.html',
+    selector: 'products',
+    directives: FORM_DIRECTIVES
 })
 export class ProductsComponent {
+    offers:Offer[] = [];
     
+    onSubmit(offer:Offer) {
+        this.offers.push(offer);
+    }
 }
