371dd9b9ad1e413d8a46a755ae1c7555d313e004
DeJw92 <dpawlak1992@gmail.com>
2016-04-24 22:43:47 +0200

    mock service replaced with backend call

--- app/home-page.ts
+++ app/home-page.ts
@@ -1,10 +1,24 @@
-import {Component} from "angular2/core";
-import {OFFERS} from "./offers-mock-service";
+import {Component, OnInit} from "angular2/core";
+import {Offer} from "./offer";
+import {Http} from "angular2/http";
 
 @Component({
     selector: 'home-page',
     templateUrl: 'views/home-page.html'
 })
-export class HomePageComponent {
-    offers = OFFERS;
+export class HomePageComponent implements OnInit{
+    offers:Offer[] = [];
+    
+    constructor(private http:Http) {}
+    
+    ngOnInit() {
+        this.http.get("http://localhost:8080/ready-offers")
+            .map(res => res.json())
+            .subscribe(
+                offers => {
+                    this.offers = offers;
+                },
+                err => console.log("Service is unavailable")
+            );
+    }
 }
--- app/offers-mock-service.ts
+++
@@ -1,2 +0,0 @@
-import {Offer} from "./offer";
-export var OFFERS: Offer[] = [new Offer("GTA 4",30), new Offer("Crysis",50), new Offer("Battlefied",40)];
